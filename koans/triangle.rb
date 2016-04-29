783660211b9def3dd1d0bc3583bd64261f7dd08d
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-04-29 20:55:21 +0100

    About triangle project 2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,9 +14,21 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  return :equilateral if a == b and b == c
-  return :scalene if a != b and b != c and a != c
+  check_triangle_sides a, b, c
+  sides = [a, b, c].uniq
+  return :equilateral if sides.count == 1
+  return :scalene if sides.count == 3
   :isosceles
+end
+
+def check_triangle_sides(a, b, c)
+  sides=[a, b, c].sort
+  if sides[0] <= 0
+    raise TriangleError, "At least one side is 0 or negative."
+  end
+  if (sides[0] + sides[1]) <= sides[2]
+    raise TriangleError, "The add of the shortest sides of the triangle must be greater than the greater side."
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
