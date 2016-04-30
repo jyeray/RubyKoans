51f2d530c0615668fb763eb56a7de162c5048bac
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-04-30 11:51:52 +0100

    About dice project

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,16 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+  def roll(times)
+    @values = Array.new
+    times.times do
+      @values << 1 + rand(6)
+    end
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
